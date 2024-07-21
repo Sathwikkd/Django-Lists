5b3a834d6b001525b9bf2385d4b1a26c467730f5
Sathwikkd <skadasur@gmail.com>
2024-07-21 16:34:46 +0530

    updated

--- displaylist/views.py
+++ displaylist/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def lists_view(request):
     fruits = ['Sebu', 'Karjuura', 'halasinahannu','baleHannu']
-    Members = ['Savithri Bhai', 'Dini', 'Shubh', 'Sathu','Ruth', 'Mr.handa', 'Mr.Chotuu']
+    Members = ['Savithri ', 'Dinesh', 'Shubha', 'Sathwik','Ruthwik', 'Mr.handa', 'Mr.Chotuu']
     context = {
         'fruits': fruits,
 
